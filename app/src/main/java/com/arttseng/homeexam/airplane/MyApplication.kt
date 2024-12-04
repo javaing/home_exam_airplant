c881cd7a754d513aacd7335330e1bbf949fc3371
javaing63 <arttseng@gmail.com>
2024-12-04 16:48:53 +0800

    更新TDX access token拿法

--- app/src/main/java/com/arttseng/homeexam/airplane/MyApplication.kt
+++ app/src/main/java/com/arttseng/homeexam/airplane/MyApplication.kt
@@ -3,13 +3,10 @@ package com.arttseng.homeexam.airplane
 import android.app.Application
 import android.util.Log
 import com.arttseng.homeexam.airplane.tools.Const
-import me.relex.circleindicator.BuildConfig
+import okhttp3.FormBody
 import okhttp3.Interceptor
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.ResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
 import org.json.JSONObject
@@ -19,6 +16,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.Body
 import retrofit2.http.POST
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 class MyApplication: Application() {
@@ -26,26 +24,26 @@ class MyApplication: Application() {
         private var instance: Application? = null
         var tdx_token = ""
         var currencyApikey = ""
-            private var okHttpClient: OkHttpClient? = null
+        private var okHttpClient: OkHttpClient? = null
         fun getOkHttpClient(): OkHttpClient {
             if(okHttpClient==null) {
                 //if (BuildConfig.DEBUG) {
-                    val interceptor = HttpLoggingInterceptor()
-                    interceptor.level = HttpLoggingInterceptor.Level.HEADERS
-                    okHttpClient = OkHttpClient.Builder()
-                        .addInterceptor(interceptor)
-                        .addInterceptor(Interceptor { chain ->
-                            val request: Request =
-                                chain.request().newBuilder()
-                                    .addHeader("Accept", "application/json")
-                                    .addHeader("Authorization", "Bearer $tdx_token")
-                                    .build()
-                            chain.proceed(request)
-                        })
-                        .retryOnConnectionFailure(true) //.addInterceptor(UserAgentInterc)
-                        .connectTimeout(80, TimeUnit.SECONDS)
-                        .readTimeout(80, TimeUnit.SECONDS)
-                        .build()
+                val interceptor = HttpLoggingInterceptor()
+                interceptor.level = HttpLoggingInterceptor.Level.HEADERS
+                okHttpClient = OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .addInterceptor(Interceptor { chain ->
+                        val request: Request =
+                            chain.request().newBuilder()
+                                .addHeader("Accept", "application/json")
+                                .addHeader("Authorization", "Bearer $tdx_token")
+                                .build()
+                        chain.proceed(request)
+                    })
+                    .retryOnConnectionFailure(true) //.addInterceptor(UserAgentInterc)
+                    .connectTimeout(80, TimeUnit.SECONDS)
+                    .readTimeout(80, TimeUnit.SECONDS)
+                    .build()
 
                 Log.e("", "art created okhttp")
             }
@@ -53,36 +51,32 @@ class MyApplication: Application() {
         }
     }
 
-    interface ApiService {
-        @POST(Const.TDX_Token)
-        fun postRequest(@Body requestBody: RequestBody?): Call<ResponseBody>
-    }
-
-    fun getTDXToken() {
-        val retrofit = Retrofit.Builder()
-            .client(getOkHttpClient())
-            .baseUrl(Const.TDX_Base)
+    private fun getTDXToken() {
+        val formBody = FormBody.Builder()
+            .add("grant_type", "client_credentials" )
+            .add("client_id", resources.getString(R.string.tdx_client_id))
+            .add("client_secret", resources.getString(R.string.tdx_client_secret))
             .build()
-        val apiService = retrofit.create(ApiService::class.java)
-        val body = mapOf(
-            "grant_type" to "client_credentials",
-            "client_id" to resources.getString(R.string.tdx_client_id),
-            "client_secret" to resources.getString(R.string.tdx_client_secret)
-        )
-        val requestBody = JSONObject(body).toString().toRequestBody("application/json".toMediaTypeOrNull())
 
-        apiService.postRequest(requestBody).enqueue(object : Callback<ResponseBody> {
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+        // creating request
+        var request = Request.Builder().url(Const.TDX_Base + Const.TDX_Token)
+            .post(formBody)
+            .build()
 
-                println("other message:" + response.body()?.string())
-                response.body().let {
-                    val res = JSONObject(it.toString())
-                    tdx_token = res.getString("access_token")
-                }
+        var client = OkHttpClient();
+        client.newCall(request).enqueue(object: okhttp3.Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                Log.e("", "okhttp fail:")
             }
 
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                // handle the failure
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                response.body.let {
+                    val jsonData: String = response.body!!.string()
+                    //Log.e("", "okhttp ok:" + jsonData)
+                    val Jobject = JSONObject(jsonData)
+                    tdx_token = Jobject.getString("access_token")
+                }
+
             }
         })
     }
@@ -90,10 +84,9 @@ class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-        tdx_token = resources.getString(R.string.tdx_token)
+
         currencyApikey = resources.getString(R.string.freecurrency_apikey)
-        if(tdx_token.isEmpty()) {
-            getTDXToken()
-        }
+        //tdx_token = resources.getString(R.string.tdx_token)
+        getTDXToken()
     }
 }
--- app/src/main/java/com/arttseng/homeexam/airplane/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/arttseng/homeexam/airplane/viewmodel/MyViewModel.kt
@@ -12,13 +12,11 @@ import com.arttseng.homeexam.airplane.datamodel.Currencies
 import com.arttseng.homeexam.airplane.datamodel.Departure
 import com.arttseng.homeexam.airplane.tools.Const
 import com.arttseng.homeexam.airplane.tools.RetrofitFactory
-import com.squareup.moshi.Moshi
 import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
     private val apiClient = RetrofitFactory.WebAccess.API_Airpane;
     private val apiCurrency = RetrofitFactory.WebAccess.APICurrency
-    //private var userPickLanguage:String
 
     val airportData = MutableLiveData<List<Airport>>();
     val departureData = MutableLiveData<List<Departure>>()
@@ -26,87 +24,9 @@ class MyViewModel : ViewModel() {
     val isNeedReload = MutableLiveData(false)
     val currenciesData = MutableLiveData<Currencies?>();
 
-    private val testStr = """
-[
-  {
-    "AirportID": "AAN",
-    "AirportName": {
-      "Zh_tw": "艾因國際機場",
-      "En": "AL AIN"
-    },
-    "AirportIATA": "AAN",
-    "AirportICAO": "OMAL",
-    "AirportPosition": {},
-    "AirportCityName": {},
-    "AirportNationality": "AE",
-    "AuthorityID": "CAA",
-    "UpdateTime": "2024-12-01T09:12:09+08:00",
-    "VersionID": 1542
-  }
-]
-    """.trimIndent()
-
-    private val dummyDeparture = """
-        [
-  {
-    "FlightDate": "2024-12-01",
-    "FlightNumber": "7016",
-    "AirlineID": "DA",
-    "DepartureAirportID": "CMJ",
-    "ArrivalAirportID": "MZG",
-    "ScheduleDepartureTime": "2024-12-01T14:25",
-    "ActualDepartureTime": "2024-12-01T14:19",
-    "DepartureRemark": "已飛",
-    "Terminal": "",
-    "Gate": "",
-    "IsCargo": false,
-    "UpdateTime": "2024-12-01T20:08:16+08:00"
-  }
-]
-    """.trimIndent()
-
-    private val dummyArrival = """
-        [
-          {
-            "FlightDate": "2024-12-01",
-            "FlightNumber": "885",
-            "AirlineID": "ZE",
-            "DepartureAirportID": "CJU",
-            "ArrivalAirportID": "TPE",
-            "ScheduleArrivalTime": "2024-12-01T00:05",
-            "ActualArrivalTime": "2024-12-01T00:00",
-            "EstimatedArrivalTime": "2024-12-01T00:00",
-            "ArrivalRemark": "已到ARRIVED",
-            "Terminal": "1",
-            "Gate": " A7",
-            "IsCargo": false,
-            "AcType": "",
-            "BaggageClaim": "03",
-            "UpdateTime": "2024-12-02T16:30:09+08:00"
-          }
-        ]
-    """.trimIndent()
-
-//    private fun genTestData(): List<Airport>? {
-//        val moshi = Moshi.Builder().build()
-//        val adapter = moshi.adapter<List<Airport>>(Airport::class.java)
-//        return adapter.fromJson(testStr)
-//    }
-
-//    private fun genDummyDeparture(): List<Departure>? {
-//        val moshi = Moshi.Builder().build()
-//        val adapter = moshi.adapter<List<Departure>>(Departure::class.java)
-//        return adapter.fromJson(dummyDeparture)
-//    }
-
-//    private fun genDummyArrival(): List<ArrivalItem>? {
-//        val moshi = Moshi.Builder().build()
-//        val adapter = moshi.adapter<List<ArrivalItem>>(ArrivalItem::class.java)
-//        return adapter.fromJson(dummyArrival)
-//    }
-
 
     init {
+        airportData.value = emptyList()
         getAirport()
     }
 
@@ -132,6 +52,7 @@ class MyViewModel : ViewModel() {
     }
 
     fun getAirport() {
+        if(MyApplication.tdx_token.isEmpty()) return
         viewModelScope.launch {
             try {
                 val response = apiClient.getAirport("JSON")
@@ -150,8 +71,8 @@ class MyViewModel : ViewModel() {
     }
 
     fun getDeparture() {
-
-        if(airportData.value?.isEmpty() == true) getAirport()
+        if(MyApplication.tdx_token.isEmpty()) return
+        if(airportData.value!!.isEmpty()) getAirport()
 
         viewModelScope.launch {
             val response = apiClient.getDeparture("TPE","JSON", Const.RecordSize, false)
@@ -165,8 +86,8 @@ class MyViewModel : ViewModel() {
     }
 
     fun getArrival() {
-
-        if(airportData.value?.isEmpty() == true) getAirport()
+        if(MyApplication.tdx_token.isEmpty()) return
+        if(airportData.value!!.isEmpty()) getAirport()
 
         viewModelScope.launch {
             val response = apiClient.getArrival("TPE","JSON", Const.RecordSize, false)
